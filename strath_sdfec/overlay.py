b141adb87ec8136a43a1dade067b3993ca9340de
LewisMcL <lewis.mclaughlin@strath.ac.uk>
2023-01-19 16:40:39 +0000

    added overlay for downloading bitstream

---
+++ strath_sdfec/overlay.py
@@ -0,0 +1,20 @@
+__author__ = "Lewis McLaughlin"
+__organisation__ = "The Univeristy of Strathclyde"
+__support__ = "https://github.com/strath-sdr/rfsoc_sdfec"
+
+from pynq import Overlay
+
+class SdfecOverlay(Overlay):
+    
+    def __init__(self, bitfile_name=None, **kwargs):
+        
+        GEN3 = ['RFSoC4x2']
+        GEN1 = ['RFSoC2x2', 'ZCU111']
+        
+        # Generate default bitfile name
+        if bitfile_name is None:
+            this_dir = os.path.dirname(__file__)
+            bitfile_name = os.path.join(this_dir, 'bitstream', 'strath_sdfec.bit')
+            
+        # Create Overlay
+        super().__init__(bitfile_name, **kwargs)
