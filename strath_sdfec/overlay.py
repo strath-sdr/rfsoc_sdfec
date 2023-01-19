73d9f97693d0fe5a9df7b6baa1a801f43e474276
LewisMcL <lewis.mclaughlin@strath.ac.uk>
2023-01-19 19:44:06 +0000

    allows for hw accelerated bitstream to be downloaded

--- strath_sdfec/overlay.py
+++ strath_sdfec/overlay.py
@@ -6,16 +6,19 @@ from pynq import Overlay
 import os
 
 class SdfecOverlay(Overlay):
-    
+
     def __init__(self, bitfile_name=None, **kwargs):
-        
+
         GEN3 = ['RFSoC4x2']
         GEN1 = ['RFSoC2x2', 'ZCU111']
-        
+
         # Generate default bitfile name
         if bitfile_name is None:
             this_dir = os.path.dirname(__file__)
             bitfile_name = os.path.join(this_dir, 'bitstream', 'strath_sdfec.bit')
+        elif bitfile_name == "hw_accel":
+            this_dir = os.path.dirname(__file__)
+            bitfile_name = os.path.join(this_dir, 'bitstream', 'strath_sdfec_hw.bit')
             
         # Create Overlay
         super().__init__(bitfile_name, **kwargs)
